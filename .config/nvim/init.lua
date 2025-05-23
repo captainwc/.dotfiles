37013ba50560b7906c4bad4005dfb459a93b860d
wddjwk <wddjwk@qq.com>
2025-05-23 12:16:22 +0800

    [nvim] 移除minisurround的错误快捷键映射，直接用gs[ardhf]即可，加上个g前缀

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,18 @@
 if vim.g.vscode then
-require("config.lazy")
-require("mini.surround").setup()
-require("vscode")
+    require("config.lazy")
+    require("vscode")
 else
+    -- bootstrap lazy.nvim, LazyVim and your plugins
+    require("config.lazy")
 
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+    -- It's NOUse now, remain here just as an example
+    -- config about mini.surround referred to plugins/minisurround.lua
+    -- require("mini.surround").setup()
 
--- config about mini.surround referred to plugins/minisurround.lua
-require("mini.surround").setup()
+    -- vim.cmd([[colorscheme catppuccin-mocha]])
+    vim.cmd([[colorscheme dracula]])
+    -- vim.cmd.colorscheme = "embark"
 
--- vim.cmd([[colorscheme catppuccin-mocha]])
-vim.cmd([[colorscheme dracula]])
--- vim.cmd.colorscheme = "embark"
-
--- 启用 true-color 支持
-vim.opt.termguicolors = true
-
+    -- 启用 true-color 支持
+    vim.opt.termguicolors = true
 end
--- .config/nvim/lua/plugins/minisurround.lua
+++ .config/nvim/lua/plugins/minisurround.lua
@@ -1,11 +1,13 @@
 return {
-    "mini.surround",
-    mappings = {
-        add = "sa", -- 添加环绕
-        delete = "sd", -- 删除环绕
-        highlight = "sh", -- 高亮环绕
-        replace = "sr", -- 替换环绕
-    },
-    n_lines = 20, -- 搜索环绕的行数
-    search_method = "cover", -- 搜索方法
+    -- minisurround 的快捷键时 gs[ardhfF]，而非只有一个s，开始不知道，为了跟跳转的区分才加了这个
+    -- 但这会影响s跳转的体验，故删除。留在此处只是为了作为一个配置的例子。获取不久后将会删除
+    -- "mini.surround",
+    -- mappings = {
+    --     add = "sa", -- 添加环绕
+    --     delete = "sd", -- 删除环绕
+    --     highlight = "sh", -- 高亮环绕
+    --     replace = "sr", -- 替换环绕
+    -- },
+    -- n_lines = 20, -- 搜索环绕的行数
+    -- search_method = "cover", -- 搜索方法
 }
