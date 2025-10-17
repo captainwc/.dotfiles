7773efa3713fa52dd7255305278390514183ad4e
shuaikai <wddjwk@qq.com>
2025-10-17 21:05:43 +0800

    [nvim] reconfig floaterm

--- .config/nvim/lua/plugins/floatterm.lua
+++ .config/nvim/lua/plugins/floatterm.lua
@@ -1,5 +1,22 @@
 return {
     {
         "voldikss/vim-floaterm",
+        init = function()
+            local default_shell = tostring(vim.fn.getenv("SHELL") or "/bin/bash")
+            if string.find(default_shell, "zsh") then
+                vim.g.floaterm_shell = default_shell
+                vim.g.floaterm_shellargs = "-i"
+            else
+                vim.g.floaterm_shell = "/bin/bash"
+                vim.g.floaterm_shellargs = "-i"
+            end
+            vim.g.floaterm_width = 0.9
+            vim.g.floaterm_height = 0.85
+            vim.g.floaterm_title = (string.find(default_shell, "zsh") and "ZSH" or "BASH") .. " ($1/$2)"
+            vim.g.floaterm_autoclose = 0
+        end,
+        -- keys = {
+        --   { "<leader>ft", "<cmd>FloatermToggle<cr>", desc = "Toggle terminal" },
+        -- },
     },
 }
