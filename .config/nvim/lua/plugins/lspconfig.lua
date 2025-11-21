bfbb05a285973e70d8eb746305329adb2b920cc9
shuaikai <wddjwk@qq.com>
2025-11-22 01:00:20 +0800

    [nvim] format lspconfig add add rust analyzer

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,32 +1,35 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = {
-      enabled = false,
-      exclude = { "python" }
-    },
+    "neovim/nvim-lspconfig",
+    opts = {
+        inlay_hints = {
+            enabled = false,
+            exclude = { "python" },
+        },
 
-    servers = {
-      clangd = {
-        cmd = {
-          "clangd",
-          "--background-index",
-          "-j=12",
-          "--clang-tidy",
-          -- "--clang-tidy-checks=performance-*,bugprone-*",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders=false",
-          "--fallback-style=Google",
-          "--all-scopes-completion",
-          "--enable-config"
+        servers = {
+            clangd = {
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "-j=12",
+                    "--clang-tidy",
+                    -- "--clang-tidy-checks=performance-*,bugprone-*",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders=false",
+                    "--fallback-style=Google",
+                    "--all-scopes-completion",
+                    "--enable-config",
+                },
+                init_options = {
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    clangdFileStatus = true,
+                },
+            },
+            rust_analyzer = {
+                standalone = true,
+            },
         },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      }
-    }
-  },
+    },
 }
